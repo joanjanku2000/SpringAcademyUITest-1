9ba24ad08ae7c312800a9e35d2ec310d31f4175c
dballa <dballa@PC-DBALLA.mshome.net>
2021-02-12 19:12:39 +0100

    Add 2 numbers

---
+++ GitTest/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+NumberUtils numberUtils=new NumberUtils();
+int result=numberUtils.addNumbers(3, 5);
+System.out.println(result);
+	}
+
+}
---
+++ GitTest/src/NumberUtils.java
@@ -0,0 +1,8 @@
+
+public class NumberUtils {
+	
+	public int addNumbers(int a, int b) {
+		return a+b;
+	}
+
+}
